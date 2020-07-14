302a488bdcf17665307bd883368102cd17866802
hiromats3 <hiromats3@gmail.com>
2020-07-15 05:28:38 +0900

    create quarkus project

---
+++ src/test/java/org/hiromats3/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package org.hiromats3;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
